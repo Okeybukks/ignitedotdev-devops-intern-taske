91c97d7f63194f3ec508e8701c66e51b347a8101
Okeybukks <achebepeter94@gmail.com>
2023-09-18 01:58:23 +0100

    add prometheus configuration

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -1,9 +1,17 @@
 provider "kubectl" {
   config_path    = "../config/KUBECONFIG" # Path to your kubeconfig file
-  config_context = "kind-kind" # Context within your kubeconfig
+  config_context = "kind-kind"            # Context within your kubeconfig
 }
 
 resource "kubectl_manifest" "hello_world_deployment" {
-    yaml_body = file("../deployment.yaml")
+  yaml_body = file("../deployment.yaml")
 }
 
+resource "kubectl_manifest" "namespace" {
+  yaml_body = <<YAML
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: monitoring
+YAML
+}
---
+++ terraform/kube-prometheus-stack.tf
@@ -0,0 +1,12 @@
+provider "helm" {
+  kubernetes {
+    config_path = "../config/KUBECONFIG"
+  }
+}
+
+resource "helm_release" "prometheus" {
+  name       = "kube-prometheus-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  namespace  = "monitoring"
+}
